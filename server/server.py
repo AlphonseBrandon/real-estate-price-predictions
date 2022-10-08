0d43f7f1a6d1f21242fda00f94eb96203f0a9033
Alphonse Brandon <alphonse.brand@gmail.com>
2022-10-08 14:48:10 +0100

    Server side completed!

--- server/server.py
+++ server/server.py
@@ -14,6 +14,18 @@ def get_location_names():
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
+# get estimated price from the client
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bath, bhk)
+    })
+
 if __name__ == '__main__':
     print('Starting python Flask server for House Price Prediction')
     app.run(debug=True, host='localhost', port=5000)
